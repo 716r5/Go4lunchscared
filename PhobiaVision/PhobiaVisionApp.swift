311fcacc5da4dc39b552e072c410a6922d4ddf0c
wei-song1 <weisong141@hotmail.com>
2025-08-06 13:18:43 +0800

    Skeleton code

---
+++ PhobiaVision/PhobiaVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhobiaVisionApp.swift
+//  PhobiaVision
+//
+//  Created by Wei Song on 6/8/25.
+//
+
+import SwiftUI
+
+@main
+struct PhobiaVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhobiaVisionTests/PhobiaVisionTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhobiaVisionTests.swift
+//  PhobiaVisionTests
+//
+//  Created by Wei Song on 6/8/25.
+//
+
+import Testing
+@testable import PhobiaVision
+
+struct PhobiaVisionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
